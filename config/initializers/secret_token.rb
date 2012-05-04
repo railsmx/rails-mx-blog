5d37da039ea29b670898a8fd131fba4c682adee0
David Padilla <david@crowdint.com>
2012-05-03 19:01:54 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMxBlog::Application.config.secret_token = '2540a2ce61e6460622038ef436066c36cc381f5921b61ac097eac9a7b653976a52d5a1c6782af00bc3a1d20a7f34f5cfd2c7ff929f59a5db3cd1e259aa18e544'
